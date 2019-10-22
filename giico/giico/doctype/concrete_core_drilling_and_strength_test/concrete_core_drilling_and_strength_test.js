2d84c74c7558015a2fbc696f533f487fb6703ac6
vhrsramya <ramya.a@voltechgroup.com>
2019-10-22 13:13:05 +0000

    updated Tests

---
+++ giico/giico/doctype/concrete_core_drilling_and_strength_test/concrete_core_drilling_and_strength_test.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, VHRS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Concrete Core Drilling And Strength Test', {
+	// refresh: function(frm) {
+
+	// }
+});
