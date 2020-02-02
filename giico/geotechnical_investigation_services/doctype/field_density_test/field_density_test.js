c43e14ad951afcc607d67dd2f0403db55053aee9
Abdulla P I <hereabdulla@gmail.com>
2020-02-02 09:00:19 +0000

    giico latest push

---
+++ giico/geotechnical_investigation_services/doctype/field_density_test/field_density_test.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, VHRS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Field Density Test', {
+	// refresh: function(frm) {
+
+	// }
+});
